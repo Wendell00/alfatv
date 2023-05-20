4aff91f479482d8acf860229c33077fea2fa747c
wendell00 <wendellborges2003@gmail.com>
2023-05-20 01:27:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SiteDeAdvocaciaEImobiliária
+ Projeto feito para uma empresa de advocacia, sem fins monetários.
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
