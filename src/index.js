17a3a348bbc2b7f5f75b3731d44cbf6562ca19fb
wendell00 <wendellborges2003@gmail.com>
2023-05-20 21:56:39 -0300

    adicionando cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // Javascript
+
+aside = document.querySelector('.aside-nav')
+menuIcon = document.querySelector('.menuIcon')
+closeIcon = document.querySelector('.closeIcon')
+
+menuIcon.addEventListener('click', () => {
+    aside.classList.toggle('dis-inline');
+});
+
+closeIcon.addEventListener('click', () => {
+    aside.classList.toggle('dis-inline');
+});
